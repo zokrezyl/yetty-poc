b080809aac61d349043bc30c13dc1cc695ac9a52
zokrezyl <zokrezyl@gmail.com>
2026-02-07 18:21:08 +0100

    fix libmagic: touch autotools files to prevent aclocal regen

--- build-tools/cmake/Libmagic.cmake
+++ build-tools/cmake/Libmagic.cmake
@@ -18,6 +18,11 @@ ExternalProject_Add(libmagic_ext
     # After initial download+build, skip update to avoid rebuilds
     UPDATE_DISCONNECTED TRUE
 
+    # Touch auto-generated files to prevent make from trying to regenerate them
+    # Tarball timestamps can trigger aclocal.m4 regeneration otherwise
+    PATCH_COMMAND
+        find <SOURCE_DIR> -name "aclocal.m4" -o -name "configure" -o -name "Makefile.in" -o -name "config.h.in" | xargs touch
+
     # Configure without our custom paths - let it find system zlib for detection
     # --disable-maintainer-mode prevents make from trying to regenerate aclocal.m4
     CONFIGURE_COMMAND
