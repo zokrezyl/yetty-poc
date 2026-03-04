64aaae69b188cea4c9a29aa534fd7fd462d6e3dc
zokrezyl <zokrezyl@gmail.com>
2026-03-04 23:40:50 +0100

    target build fixes

--- build-tools/cmake/openssl.cmake
+++ build-tools/cmake/openssl.cmake
@@ -1,32 +1,48 @@
-# OpenSSL built from source for ALL platforms
-# Linux, macOS, Windows, Android, Emscripten
+# OpenSSL configuration for all platforms
+# - Desktop (Linux, macOS, Windows): Use system OpenSSL or build from source
+# - Android: Build from source
+# - WebAssembly: Build from source (Emscripten compatible)
 
-CPMAddPackage(
-    NAME openssl
-    GITHUB_REPOSITORY janbar/openssl-cmake
-    GIT_TAG 1.1.1w-20250419
-    OPTIONS
-        "BUILD_SHARED_LIBS OFF"
-)
-
-# Create OpenSSL::Crypto target that cpr/curl expect
-if(NOT TARGET OpenSSL::Crypto)
-    add_library(OpenSSL::Crypto INTERFACE IMPORTED GLOBAL)
-    target_link_libraries(OpenSSL::Crypto INTERFACE crypto)
-    target_include_directories(OpenSSL::Crypto INTERFACE "${openssl_SOURCE_DIR}/include")
+# Try to find system OpenSSL first (works on most desktop platforms)
+if(NOT EMSCRIPTEN AND NOT YETTY_ANDROID)
+    find_package(OpenSSL QUIET)
 endif()
 
-# Create OpenSSL::SSL target that cpr/curl expect
-if(NOT TARGET OpenSSL::SSL)
-    add_library(OpenSSL::SSL INTERFACE IMPORTED GLOBAL)
-    target_link_libraries(OpenSSL::SSL INTERFACE ssl OpenSSL::Crypto)
-    target_include_directories(OpenSSL::SSL INTERFACE "${openssl_SOURCE_DIR}/include")
-endif()
+if(OPENSSL_FOUND)
+    message(STATUS "Using system OpenSSL: ${OPENSSL_VERSION}")
+else()
+    message(STATUS "Building OpenSSL from source (janbar/openssl-cmake)")
+    
+    # Use janbar/openssl-cmake with CMake policy workaround
+    # Pin to a specific commit known to work
+    CPMAddPackage(
+        NAME openssl
+        GITHUB_REPOSITORY janbar/openssl-cmake
+        GIT_TAG 1.1.1w-20250419
+        OPTIONS
+            "BUILD_SHARED_LIBS OFF"
+            "WITH_APPS OFF"
+    )
 
-# Set variables for find_package(OpenSSL) compatibility
-set(OPENSSL_FOUND TRUE CACHE BOOL "" FORCE)
-set(OPENSSL_INCLUDE_DIR "${openssl_SOURCE_DIR}/include" CACHE PATH "" FORCE)
-set(OPENSSL_CRYPTO_LIBRARY OpenSSL::Crypto CACHE STRING "" FORCE)
-set(OPENSSL_SSL_LIBRARY OpenSSL::SSL CACHE STRING "" FORCE)
-set(OPENSSL_LIBRARIES OpenSSL::SSL OpenSSL::Crypto CACHE STRING "" FORCE)
-set(OPENSSL_VERSION "1.1.1w" CACHE STRING "" FORCE)
+    # Create OpenSSL::Crypto target that cpr/curl expect
+    if(NOT TARGET OpenSSL::Crypto)
+        add_library(OpenSSL::Crypto INTERFACE IMPORTED GLOBAL)
+        target_link_libraries(OpenSSL::Crypto INTERFACE crypto)
+        target_include_directories(OpenSSL::Crypto INTERFACE "${openssl_SOURCE_DIR}/include")
+    endif()
+
+    # Create OpenSSL::SSL target that cpr/curl expect
+    if(NOT TARGET OpenSSL::SSL)
+        add_library(OpenSSL::SSL INTERFACE IMPORTED GLOBAL)
+        target_link_libraries(OpenSSL::SSL INTERFACE ssl OpenSSL::Crypto)
+        target_include_directories(OpenSSL::SSL INTERFACE "${openssl_SOURCE_DIR}/include")
+    endif()
+
+    # Set variables for find_package(OpenSSL) compatibility
+    set(OPENSSL_FOUND TRUE CACHE BOOL "" FORCE)
+    set(OPENSSL_INCLUDE_DIR "${openssl_SOURCE_DIR}/include" CACHE PATH "" FORCE)
+    set(OPENSSL_CRYPTO_LIBRARY OpenSSL::Crypto CACHE STRING "" FORCE)
+    set(OPENSSL_SSL_LIBRARY OpenSSL::SSL CACHE STRING "" FORCE)
+    set(OPENSSL_LIBRARIES OpenSSL::SSL OpenSSL::Crypto CACHE STRING "" FORCE)
+    set(OPENSSL_VERSION "1.1.1w" CACHE STRING "" FORCE)
+endif()
